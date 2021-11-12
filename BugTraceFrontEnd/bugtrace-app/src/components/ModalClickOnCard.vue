2ae67cad78d176e31f3864b17bf92d03f2f458a7
Kuraido <serelisltu@gmail.com>
2021-11-12 23:59:41 +0000

    Backend:      •Implemented ModifyCardController and ModifyCardService, which gives       ability to modify cards, move form todo to InProgress and from       InProgress to complete

--- BugTraceFrontEnd/bugtrace-app/src/components/ModalClickOnCard.vue
+++ BugTraceFrontEnd/bugtrace-app/src/components/ModalClickOnCard.vue
@@ -1,36 +1,33 @@
 <template>
 
-  <div class="myBackground">
-    <div class="container">
+  <div class="myBackground" @click.self="close">
+    <div class="container" >
     <div class="row">
-      <div class="col-3"></div>
+      <div @click.self="close" class="col-3"></div>
       <div class="col-6">
-        <div class="myModal">
-          <h5 class="card-title">Crashing Zoom</h5>
+        <div  class="myModal">
+          <h5 class="card-title">{{ card.title }}</h5>
           <div class="myAuthorAndData">
-            <h6 class="card-subtitle mb-2 text-muted" style="font-size: 0.8em">Created by: Klaidas Serelis<br>
-              Created at: 11/11/2011
+            <h6 class="card-subtitle mb-2 text-muted" style="font-size: 0.8em">Created by: {{ card.creator }}<br>
+              Created at: {{ card.dateCreated }}
             </h6>
           </div>
           <div class="myAuthorAndData">
-            <h6 class="card-subtitle mb-2 text-muted" style="font-size: 0.8em">Created by: Klaidas Serelis<br>
-              Created at: 11/11/2011
+            <h6 class="card-subtitle mb-2 text-muted" style="font-size: 0.8em">Assigned to: {{ card.assignedTo }}<br>
+              Date assigned: {{card.dateAssigned}}
             </h6>
           </div>
           <div class="myAuthorAndData">
-            <h6 class="card-subtitle mb-2 text-muted" style="font-size: 0.8em">Created by: Klaidas Serelis<br>
-              Created at: 11/11/2011
+            <h6 class="card-subtitle mb-2 text-muted" style="font-size: 0.8em">Completed by: {{ card.completedBy }}<br>
+              Date completed: {{card.dateCompleted}}
             </h6>
           </div>
           <div class="myDescription">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis corporis cumque dolor, ex iste itaque iure libero magnam mollitia natus neque omnis, praesentium quaerat quisquam suscipit. Alias aperiam architecto, aspernatur commodinatus neque omnis, praesentium quaerat quisquam suscipit. Alias aperiam architecto, aspernatur commodi debitis deleniti deserunt dicta eveniet hic illum laudantium magnam nemo nulla numquam officia, possimus quidem similique soluta soluta soluta aa, veniam vitae.</p>
+            <p>{{card.description}}</p>
           </div>
           <div class="myPills">
-            <span class="badge rounded-pill bg-danger myPill">Danger</span>
-            <span class="badge rounded-pill bg-primary myPill">Primary</span>
-            <span class="badge rounded-pill bg-primary myPill">Primary</span>
-            <span class="badge rounded-pill bg-primary myPill">Primary</span>
-            <span class="badge rounded-pill bg-primary myPill">Primary</span>
+            <span class="badge rounded-pill bg-danger myPill">{{ card.impact }}</span>
+            <span v-for="keyword in card.keywords" class="badge rounded-pill bg-primary myPill">{{keyword}}</span>
           </div>
           <div class="btn-group float-sm-end">
             <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -54,7 +51,17 @@
 
 <script>
 export default {
-  name: "ModalClickOnCard"
+  name: "ModalClickOnCard",
+  props:["card"],
+  created() {
+
+  },
+  methods:{
+    close()
+    {
+      this.$emit("close")
+    }
+  }
 }
 </script>
 
